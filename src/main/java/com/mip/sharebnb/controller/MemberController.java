1d87c4396b8527db1d79e2f59061636e4f050a0a
FoeverNa <foevna@naver.com>
2021-02-18 20:28:04 +0900

    test : CORS Test

--- src/main/java/com/mip/sharebnb/controller/MemberController.java
+++ src/main/java/com/mip/sharebnb/controller/MemberController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-@CrossOrigin
+//@CrossOrigin
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
