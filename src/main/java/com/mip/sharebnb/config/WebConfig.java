7ba625e380eea1cc30c80d91e78ee41b68bc169f
FoeverNa <foevna@naver.com>
2021-02-18 20:58:41 +0900

    test : CORS Test

--- src/main/java/com/mip/sharebnb/config/WebConfig.java
+++ src/main/java/com/mip/sharebnb/config/WebConfig.java
@@ -1,17 +1,16 @@
-//package com.mip.sharebnb.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+package com.mip.sharebnb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
 //                .exposedHeaders("Authorization");
-//
-//    }
-//}
+    }
+}
