1190095978814a6f3f5516310c30cc9deb7ad4b6
FoeverNa <foevna@naver.com>
2021-02-26 02:25:10 +0900

    test : google login test

--- src/main/java/com/sweetypie/sweetypie/controller/AuthController.java
+++ src/main/java/com/sweetypie/sweetypie/controller/AuthController.java
@@ -27,7 +27,7 @@ public class AuthController {
         return ResponseEntity.ok(new TokenDto(token));
     }
 
-    @GetMapping("/login/google/oauth2callback")
+    @GetMapping("/login/google")
     public ResponseEntity<Map<String, String>> googleLogin(@RequestParam(value = "code") String authCode) throws JsonProcessingException {
 
         Map<String, String> map;
